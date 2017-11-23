5b1e508ba46dc19060cd6a3f313a6c7503f81134
singh1114 <ranvir.singh1114@gmail.com>
2017-11-23 19:51:21 +0530

    Added models

--- quoraapp/models.py
+++ quoraapp/models.py
@@ -2,5 +2,32 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Question(models.Model):
+	user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+   	Question = models.CharField(
+   		max_length=2000,
+	)
+
+class Answer(models.Model):
+	answertext = models.CharField(
+        max_length=2000,
+    )
+	upvotes = models.BooleanField()
+
+class AnswerUser(models.Model):
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE
+	)
+	answer = models.ForeignKey(
+		Answer,
+		on_delete=models.CASCADE
+	)
+
+
